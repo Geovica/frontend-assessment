06846c3201e844bf1cbc9164ea19c55632a7c5d0
Geovica <fgeovic@gmail.com>
2021-07-20 22:34:11 +0800

    addng exercise two

---
+++ src/views/Exe-2.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-container
+    fluid
+    class="content d-flex flex-column justify-content-center align-item-center mb-3">
+      <b-row>
+          <b-col class="d-flex justify-content-center align-item-center">
+            <Sections class="section"/>
+            <Accordion class="accordion"/>
+          </b-col>
+      </b-row>
+  </b-container>
+</template>
+
+<script>
+import Sections from '../components/Sections.vue'
+import Accordion from '../components/Accordion.vue'
+
+export default {
+  name: 'Exe-2',
+  components: {
+    Sections,
+    Accordion
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  html,body{
+    margin:0;
+  }
+  .content {
+    height:85vh;
+  }
+  .accordion {
+    display: none;
+  }
+  @media (max-width: 445px) {
+    .accordion {
+      display: block;
+    }
+    .section {
+      display: none;
+    }
+  }
+</style>
