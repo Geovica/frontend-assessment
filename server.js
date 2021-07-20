294169fd85f733698b01f41d2ea3f8bbdcd557c2
Geovica <fgeovic@gmail.com>
2021-07-20 22:48:57 +0800

    adding server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
- const express = require('express')
- const serveStatic = require('serve-static')
- const patch= require('path')
- const app = express()
-
- app.use('/', serveStatic(path.join(__dirname,'/dist')))
-
- const port = process.env.PORT || 8080
- app.listed(port)
-
- console.log('Listening on port:' + port)
-
- 
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
