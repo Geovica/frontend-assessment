bed37510ab7dc20cf6ab4058ab58607bf8461a03
Geovica <fgeovic@gmail.com>
2021-07-20 22:34:00 +0800

    adding exercise one

---
+++ src/views/Exe-1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <Card />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Card from '../components/Card.vue'
+
+export default {
+  name: 'Exe-1',
+  components: {
+    Header,
+    Card
+  }
+}
+</script>
