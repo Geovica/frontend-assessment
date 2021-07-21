9754750e2bf76ce7651b464a869d06bfa113a425
Geovica <fgeovic@gmail.com>
2021-07-21 09:52:30 +0800

    adding google fonts

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -49,8 +49,6 @@ export default {
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;700&display=swap');
 
-   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;700&display=swap');
-
   body {
     font-family: 'Montserrat', sans-serif;
   }
